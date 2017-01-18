c3611d9bc24aa6dd73fec468f62cb41552da6e5b
keely glenn and Sara Frandsen <keely@>
2017-01-18 13:17:07 -0800

    created a stress survey

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,20 @@ $(document).ready(function() {
       var copeInput = $(this).val();
       copeCount++;
     });
-  });
 
+    var result = function() {
+      if(stressCount <= 2 && copeCount > 1) {
+        return "Keep doing whatever you're doing!";
+      } else if(stressCount > 2 && copeCount <= 1) {
+        return "There's no hope for you.";
+      } else {
+        return "See your doctor and don't ask CS students.";
+      }
+    };
+
+    $("#response").text(result);
+
+    $("#answer").show();
+    $("#stressSurvey").hide();
+  });
 });
